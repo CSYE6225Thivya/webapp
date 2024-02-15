3f6d1368d494b17c4e63ae2bd7e5b53113f58f50
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-02-14 20:56:04 -0500

    Added workflow for integration test

--- models/healthzModel.js
+++ models/healthzModel.js
@@ -6,9 +6,7 @@ import { Buffer } from "buffer";
 // Create Sequelize instance to connect to DB
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  // dialect: dbConfig.dialect,
-  dialect: mysql,
-
+  dialect: dbConfig.dialect,
   logging: false,
   define: {
     timestamps: false, 
