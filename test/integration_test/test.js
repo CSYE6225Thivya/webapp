0ccc24ff6853db301ccff64fc61ac3c2ac777a06
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-02-15 01:53:38 -0500

    Added workflow for integration test

--- test/integration_test/test.js
+++ test/integration_test/test.js
@@ -1,50 +1,58 @@
-import request from "supertest";
-import app from "../../server.js";
+import axios from "axios";
 import { expect } from "chai";
+
+axios.defaults.baseURL = "http://localhost:8080";
 
 describe("User Endpoint Integration Tests", () => {
   it("should create an account and validate its existence with GET", async () => {
     // Send a POST request to create a new user
-    const createUserResponse = await request(app)
-      .post("/v1/user")
-      .send({
-        first_name: "test",
-        last_name: "test",
-        username: "test1@example.com",
-        password: "test",
-      });
+    const createUserResponse = await axios.post("/v1/user", {
+      first_name: "test",
+      last_name: "test",
+      username: "test111@example.com",
+      password: "test",
+    });
     // Expect response status code to be 201
     expect(createUserResponse.status).to.equal(201);
-    const userId = createUserResponse.body.id;
+    const userId = createUserResponse.data.id;
     // Authenticate
     const authHeader = `Basic ${Buffer.from(
-      "test1@example.com:test"
+      "test111@example.com:test"
     ).toString("base64")}`;
     // Send a GET request
-    const getUserResponse = await request(app)
-      .get(`/v1/user/self`)
-      .set("Authorization", authHeader);
+    const getUserResponse = await axios.get("/v1/user/self", {
+      headers: {
+        Authorization: authHeader,
+      },
+    });
     // Expect the response status code to be 200 (OK)
     expect(getUserResponse.status).to.equal(200);
-    expect(getUserResponse.body.id).to.equal(userId);
+    expect(getUserResponse.data.id).to.equal(userId);
   });
 
   it("should update an account and validate the changes with GET", async () => {
     // Authenticate
     const authHeader = `Basic ${Buffer.from(
-      "test1@example.com:test"
+      "test111@example.com:test"
     ).toString("base64")}`;
     // Send a PUT request
-    const updateUserResponse = await request(app)
-      .put(`/v1/user/self`)
-      .set("Authorization", authHeader)
-      .send({first_name: 'testnew', last_name: 'testnew', password: 'test'});
+    const updateUserResponse = await axios.put("/v1/user/self", {
+      first_name: 'testnew',
+      last_name: 'testnew',
+      password: 'test'
+    }, {
+      headers: {
+        Authorization: authHeader,
+      },
+    });
     expect(updateUserResponse.status).to.equal(204);
     // Send a GET request
-    const getUserResponse = await request(app)
-      .get(`/v1/user/self`)
-      .set("Authorization", authHeader);
+    const getUserResponse = await axios.get("/v1/user/self", {
+      headers: {
+        Authorization: authHeader,
+      },
+    });
     expect(getUserResponse.status).to.equal(200);
-    expect(getUserResponse.body.first_name).to.equal("testnew");
+    expect(getUserResponse.data.first_name).to.equal("testnew");
   });
 });
